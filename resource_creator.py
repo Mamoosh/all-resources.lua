7444e2c6c4e2cc42ab74cf587628c63e845ea93c
U3F <98705330+Mamoosh@users.noreply.github.com>
2024-01-22 04:16:08 +0330

    Update resource_creator.py

--- resource_creator.py
+++ resource_creator.py
@@ -1,41 +1,48 @@
 import os
 
-print("resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'")
-print("\n\nfiles {\n")
+path = 'C:\\fivem\\CFXDefault_1BFEF6.base\\resources\\[ unbrand]\\cars\\'
+lua_file_path = 'C:\\__resource.lua'
+
+
 def print_ssubfolders(path):
+    output = ""
     for root, dirs, files in os.walk(path, topdown=False):
         for name in dirs:
-            stream  = (os.path.join(root, name)).replace(path, '') + '\\'
-            stream = stream.replace("\\" , "/")
-            print("'" + stream + 'vehicles.meta' + "',")
-            print("'" + stream + 'carvariations.meta' + "',")
-            print("'" + stream + 'carcols.meta' + "',")
-            print("'" + stream + 'handling.meta' + "',")
-            print("'" + stream + 'dlctext.meta' + "',")
-            print("'" + stream + 'contentunlocks.meta' + "',")
-            print("'" + stream + 'vehiclelayouts.meta' + "',")
-            print("'" + stream + 'shop_vehicle.meta' + "',")
-            print("\n")
+            stream = (os.path.join(root, name)).replace(path, '') + '\\'
+            stream = stream.replace("\\", "/")
+            output += "'" + stream + 'vehicles.meta' + "',\n"
+            output += "'" + stream + 'carvariations.meta' + "',\n"
+            output += "'" + stream + 'carcols.meta' + "',\n"
+            output += "'" + stream + 'handling.meta' + "',\n"
+            output += "'" + stream + 'dlctext.meta' + "',\n"
+            output += "'" + stream + 'contentunlocks.meta' + "',\n"
+            output += "'" + stream + 'vehiclelayouts.meta' + "',\n"
+            output += "'" + stream + 'shop_vehicle.meta' + "',\n\n"
+
+    with open(lua_file_path, 'w') as lua_file:
+        lua_file.write("resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'\n\nfiles {\n")
+        lua_file.write(output)
+        lua_file.write("}\n")
+
 
 def print_subfolders(path):
+    output = ""
     for root, dirs, files in os.walk(path, topdown=False):
         for name in dirs:
-            stream  = (os.path.join(root, name)).replace(path, '') + '\\'
-            stream = stream.replace("\\" , "/")
-            print("data_file 'HANDLING_FILE' '" + stream + "handling.meta'")
-            print("data_file 'VEHICLE_METADATA_FILE' '" + stream + "vehicles.meta'")
-            print("data_file 'CARCOLS_FILE' '" + stream + "carcols.meta'")
-            print("data_file 'VEHICLE_VARIATION_FILE' '" + stream + "carvariations.meta'")
-            print("data_file 'DLCTEXT_FILE' '" + stream + "dlctext.meta'")
-            print("data_file 'CONTENT_UNLOCKING_META_FILE' '" + stream + "contentunlocks.meta'")
-            print("data_file 'VEHICLE_LAYOUTS_FILE' '" + stream + "vehiclelayouts.meta'")
-            print("data_file 'VEHICLE_SHOP_DLC_FILE' '" + stream + "shop_vehicle.meta'")
-            print("\n")
+            stream = (os.path.join(root, name)).replace(path, '') + '\\'
+            stream = stream.replace("\\", "/")
+            output += "data_file 'HANDLING_FILE' '" + stream + "handling.meta'\n"
+            output += "data_file 'VEHICLE_METADATA_FILE' '" + stream + "vehicles.meta'\n"
+            output += "data_file 'CARCOLS_FILE' '" + stream + "carcols.meta'\n"
+            output += "data_file 'VEHICLE_VARIATION_FILE' '" + stream + "carvariations.meta'\n"
+            output += "data_file 'DLCTEXT_FILE' '" + stream + "dlctext.meta'\n"
+            output += "data_file 'CONTENT_UNLOCKING_META_FILE' '" + stream + "contentunlocks.meta'\n"
+            output += "data_file 'VEHICLE_LAYOUTS_FILE' '" + stream + "vehiclelayouts.meta'\n"
+            output += "data_file 'VEHICLE_SHOP_DLC_FILE' '" + stream + "shop_vehicle.meta'\n\n"
+
+    with open(lua_file_path, 'a') as lua_file:
+        lua_file.write(output)
 
 
-
-path = 'D:\\fivem\\CFXDefault_1BFEF6.base\\resources\\[ unbrand]\\cars\\'
 print_ssubfolders(path)
-print("}\n")
 print_subfolders(path)
-
